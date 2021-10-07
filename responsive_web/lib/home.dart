3f9404eebcf0d3bc714e2d2c9404b9b985ad846e
Junior Safera <juniorsafera.dev@gmail.com>
2021-10-06 22:01:41 -0300

    appBar

--- responsive_web/lib/home.dart
+++ responsive_web/lib/home.dart
@@ -1,5 +1,7 @@
  import 'package:flutter/material.dart';
 
+import 'widget/mobile_appbar.dart';
+
  class Home extends StatefulWidget {
    const Home({ Key? key }) : super(key: key);
  
@@ -10,9 +12,18 @@
  class _HomeState extends State<Home> {
    @override
    Widget build(BuildContext context) {
-     return Scaffold(
-            appBar: AppBar(title: Text('Layout Builder'),),
-            
-     );
+     return  LayoutBuilder(
+       builder: (context, constraint){
+
+         var largura = constraint.maxWidth;
+         var alturaBarra = AppBar().preferredSize.height;
+
+         return Scaffold(
+           appBar: largura < 600
+           ? PreferredSize(child: MobileAppBar(), preferredSize: Size(largura, alturaBarra))
+           : AppBar(),
+         );
+
+     });
    }
  }
---
+++ responsive_web/lib/widget/mobile_appbar.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class MobileAppBar extends StatefulWidget {
+  const MobileAppBar({ Key? key }) : super(key: key);
+
+  @override
+  _MobileAppBarState createState() => _MobileAppBarState();
+}
+
+class _MobileAppBarState extends State<MobileAppBar> {
+  @override
+  Widget build(BuildContext context) {
+    return  
+    AppBar(
+      title: Image.asset(
+        "imagens/logo.png",
+        fit: BoxFit.contain,
+        ),
+        actions: [
+
+          IconButton(
+            onPressed: (){},
+            icon: Icon(Icons.search)),
+
+          IconButton(
+            onPressed: (){},
+            icon: Icon(Icons.shopping_cart_rounded)),
+          
+          IconButton(
+            onPressed: (){},
+            icon: Icon(Icons.more_vert)),
+          
+
+
+        ],
+        )
+    ;
+  }
+}
