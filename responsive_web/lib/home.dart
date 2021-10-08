67e48f7ddeb1c0e045430d249a6174f24b0a0c33
Junior Safera <juniorsafera.dev@gmail.com>
2021-10-08 17:02:03 -0300

    update appbar

--- responsive_web/lib/home.dart
+++ responsive_web/lib/home.dart
@@ -12,19 +12,30 @@ import 'widgets/web_appbar.dart';
  
  class _HomeState extends State<Home> {
    @override
-   Widget build(BuildContext context) {
-     return  LayoutBuilder(
-       builder: (context, constraint){
+     Widget build(BuildContext context) {
+       return  LayoutBuilder
+       ( // Início LayoutBuilder
+         builder: (context, constraint)
+        {    // Início builder
 
-         var largura = constraint.maxWidth;
-         var alturaBarra = AppBar().preferredSize.height;
+           var largura = constraint.maxWidth;
+           var alturaBarra = AppBar().preferredSize.height;
 
-         return Scaffold(
-           appBar: largura < 600
-           ? PreferredSize(child: MobileAppBar(), preferredSize: Size(largura, alturaBarra))
-           : PreferredSize(child: WebAppBar(), preferredSize: Size(largura, alturaBarra)),
-         );
-
-     });
+          return Scaffold(  // Início Scaffold
+          // se largura for menor que 600   
+            appBar: largura < 600 ?           
+             PreferredSize( // Início PreferredSize
+              child: MobileAppBar(),
+               preferredSize: Size(largura, alturaBarra)
+                           ) // Fim PreferredSize
+                // se não 
+            : PreferredSize( // Início PreferredSize
+              child: WebAppBar(), 
+              preferredSize: Size(largura, alturaBarra)
+                           ),// Fim PreferredSize
+         ); // Fim Scaffold
+      }   // Fim builder
+     ); // Fim LayoutBuilder
+   
    }
  }
