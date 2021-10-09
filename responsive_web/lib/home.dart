a80157350309a797dba332016b6478e129724b99
Junior Safera <juniorsafera.dev@gmail.com>
2021-10-09 09:02:35 -0300

    preparando body

--- responsive_web/lib/home.dart
+++ responsive_web/lib/home.dart
@@ -1,41 +1,63 @@
- import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
 import 'widgets/mobile_appbar.dart';
 import 'widgets/web_appbar.dart';
 
- class Home extends StatefulWidget {
-   const Home({ Key? key }) : super(key: key);
- 
-   @override
-   _HomeState createState() => _HomeState();
- }
- 
- class _HomeState extends State<Home> {
-   @override
-     Widget build(BuildContext context) {
-       return  LayoutBuilder
-       ( // Início LayoutBuilder
-         builder: (context, constraint)
-        {    // Início builder
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
 
-           var largura = constraint.maxWidth;
-           var alturaBarra = AppBar().preferredSize.height;
+  @override
+  _HomeState createState() => _HomeState();
+}
 
-          return Scaffold(  // Início Scaffold
-          // se largura for menor que 600   
-            appBar: largura < 600 ?           
-             PreferredSize( // Início PreferredSize
-              child: MobileAppBar(),
-               preferredSize: Size(largura, alturaBarra)
-                           ) // Fim PreferredSize
-                // se não 
-            : PreferredSize( // Início PreferredSize
-              child: WebAppBar(), 
-              preferredSize: Size(largura, alturaBarra)
-                           ),// Fim PreferredSize
-         ); // Fim Scaffold
-      }   // Fim builder
-     ); // Fim LayoutBuilder
-   
-   }
- }
+class _HomeState extends State<Home> {
+  // Métodos
+  _ajustarVisualizacao(double larguraTela) {
+    int colunas = 2;
+    if (larguraTela <= 600) {
+      colunas = 2;
+    } else if (larguraTela <= 960) {
+      colunas = 4;
+    } else {
+      colunas = 6;
+    }
+    return colunas;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(// Início LayoutBuilder
+        builder: (context, constraint) {
+      // Início builder
+
+      var largura = constraint.maxWidth;
+      var alturaBarra = AppBar().preferredSize.height;
+
+      return Scaffold(
+        // Início Scaffold
+        // se largura for menor que 600
+        appBar: largura < 600
+            ? PreferredSize(
+                // Início PreferredSize
+                child: MobileAppBar(),
+                preferredSize: Size(largura, alturaBarra)) // Fim PreferredSize
+            // se não
+            : PreferredSize(
+                // Início PreferredSize
+                child: WebAppBar(),
+                preferredSize: Size(largura, alturaBarra)), // Fim PreferredSize
+
+        body: Padding(
+          padding: EdgeInsets.all(16),
+          child: GridView.count(
+            crossAxisCount: _ajustarVisualizacao(largura),
+            crossAxisSpacing: 8, // espaço lateral
+            mainAxisSpacing: 8, // espaço altura
+            children: [],
+          ),
+        ),
+      ); // Fim Scaffold
+    } // Fim builder
+        ); // Fim LayoutBuilder
+  }
+}
